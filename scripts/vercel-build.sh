48f94cd36e23e63a42b41b313d8d25aea097a067
Pablo Thobias <Pablo.Carminatti@cinplex.com>
2025-02-26 02:26:26 -0300

    refactor: adjusting vercel build file

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -4,6 +4,10 @@
 echo "🔍 Optimizing fonts..."
 node scripts/optimize-fonts.mjs
 
+#formatting the code with prettier and eslint
+echo "🧹 Formating the code..."
+npm run fix-all
+
 # Run the regular build
 echo "🏗️ Building application..."
 next build
